c04446a971c630fb510cdfd44fce245d38c58d8c
goodsie <mgoodie@outlook.com>
2025-09-17 12:22:44 -0400

    creating a colection with schema validation added. Brainstorm time=> databse.

--- main/mongodb/MongoConnect.mjs
+++ main/mongodb/MongoConnect.mjs
@@ -1,20 +1,24 @@
 import { MongoClient, MongoServerError, ReadPreference } from 'mongodb';
-import { students } from './schemas/students.mjs' // test object from docs
+
 
 
 class MongoConnect {
-  
+
   /**
    * The url or "Connection String" should follow formats as specified at:
    * https://www.mongodb.com/docs/manual/reference/connection-string-examples/#connection-string-examples
    * 
-   * @param {string} url 
+   * @param {string} url connection string
+   * @param {string | null} db database name to use if not specifying in connection string
    */
-  constructor(url) {
-    if (!url) throw new Error("Missing Constructor arg");
+  constructor(url, db = null) {
+    if (!url) throw new Error("Missing connection string.");
 
     this.url = this.isMongoString(url);
     this.client = new MongoClient(this.url);
+    this.db = db !== null
+      ? this.client.db(db)
+      : this.client.db();
 
   }
 
@@ -28,41 +32,47 @@ class MongoConnect {
     throw new Error("Unrecognized connection string..");
   };
 
-
 };
 
 
 class Mongo extends MongoConnect {
-  
+
   /**
    * The url or "Connection String" should follow formats as specified at:
    * https://www.mongodb.com/docs/manual/reference/connection-string-examples/#connection-string-examples
    * 
-   * @param {string} url 
+   * @param {string} url connection string
+   * @param {string | null} db database name to use if not specifying in connection string
    */
-  constructor(url){
-    super(url);
-    
+  constructor(url, db = null) {
+    db === null
+      ? super(url)
+      : super(url, db);
   }
 
   /**
+   * Creates a collection with schema validation.
    * 
-   * @param {string} db database to use
-   * @param {Object<any>} schema vlidator object
+   * @param {string} collection collection name to use when creating
+   * @param {Object<any>} schema validator object
+   * @returns the successfully created collection or throws
    */
-  async addSchema(db, schema) {
-    this.client.db(db).createCollection(schema)
+  async createSchemaCollection(collection, schema) {
+    try {
+      return await this.db.createCollection(collection, schema);
+    } catch (e) {
+      console.log(e);
+      throw new Error("Failed to create collection!")
+    }
   };
 
-
   /**
-   * 
    * @returns a client object
+   * @deprecated i'm not likely to use this anymore.
    */
   async getClient() {
     return this.client;
   };
-
 
 };
 export default Mongo;
@@ -84,63 +94,70 @@ export default Mongo;
  *       dont forget to rem before using class...
  */
 
+// import { students } from './schemas/students.mjs' // test object from docs
 
-
-// const mongo = new Mongo("mongodb://localhost:27017");
+// const mongo = new Mongo("mongodb://localhost:27017", 'mine');
+// const mongo = new Mongo("mongodb://localhost:27017");// no db in connection string uses 'test' by default.
 
 // try {
-//   await mongo.client.connect();
 
-//   /**
-//    * @type {ReadPreference}
-//    */
-//   const
-//     readPreference/*: ReadPreference*/ = [{
-//       mode: 'primary',
-//       // tags: undefined,
-//       // hedge: undefined,
-//       // maxStalenessSeconds: undefined,
-//       // minWireVersion: undefined
-//     }],
+//   await mongo.createSchemaCollection('students', students);
+  // console.log(collection);
+  // console.log(mongo.client.eventNames()); // returns array of eventsbeing listened for. comes stock with the error event.  xD
 
-//     dbOptions = {
-//       raw: false,
-//       useBigInt64: false,
-//       promoteLongs: true,
-//       promoteValues: true,
-//       promoteBuffers: false,
-//       ignoreUndefined: false,
-//       bsonRegExp: false,
-//       serializeFunctions: false, // TODO: explore. does this actually keep functions in objects ?? and does it auto deserialize when grabbing datas?
-//       fieldsAsRaw: {}, // empty by default.
-//       enableUtf8Validation: true,
-//       timeoutMS: 5000,
-//       readPreference // readPreference: [ReadPreference]
-//     },
-//     collection = mongo.client.db('local', dbOptions).collection('local');
+  
+  // /**
+  //  * @type {ReadPreference}
+  //  */
+  // const
+  //   readPreference/*: ReadPreference*/ = [{
+  //     mode: 'primary',
+  //     // tags: undefined,
+  //     // hedge: undefined,
+  //     // maxStalenessSeconds: undefined,
+  //     // minWireVersion: undefined
+  //   }],
 
-//   console.log("Collection Output: ", collection);
+  //   dbOptions = {
+  //     raw: false,
+  //     useBigInt64: false,
+  //     promoteLongs: true,
+  //     promoteValues: true,
+  //     promoteBuffers: false,
+  //     ignoreUndefined: false,
+  //     bsonRegExp: false,
+  //     serializeFunctions: false, // TODO: explore. does this actually keep functions in objects ?? and does it auto deserialize when grabbing datas?
+  //     fieldsAsRaw: {}, // empty by default.
+  //     enableUtf8Validation: true,
+  //     timeoutMS: 5000,
+  //     readPreference // readPreference: [ReadPreference]
+  //   },
+  //   collection = mongo.client.db('local', dbOptions).collection('students');
 
-//   // uncomment both inserts to trigger mangoserver error catch, just one to make loggables
-//   // await collection.insertOne({ _id: 1, label: "title" });
-//   // await collection.insertOne({ _id: 1, label: "title" });
+  // console.log("Collection Output: ", collection);
 
-//   // this only errs if duplicate is sequential. later added duplicates seem to get a new object id.. 
-//   // TODO: look deeper and make insert handler to manage this if needed.
-//   //    },
-//   //    {
-//   //      _id: new ObjectId('68c9b7c156eb17a2b998fcec'),
-//   //      id: 1,
-//   //      label: 'title'
-//   //    },
-//   //    {
-//   //      _id: new ObjectId('68c9b93af4f6bbb806ae0f21'),
-//   //      id: 1,
-//   //      label: 'title'
-//   //    },
+  // uncomment both inserts to trigger mangoserver error catch, just one to make loggables
+  // await collection.insertOne({ _id: 1, label: "title" });
+  // await collection.insertOne({ _id: 1, label: "title" });
 
-//   const findResult = await collection.find().toArray();// didnt need the empty object
-//   console.log('Found documents =>', findResult);
+  // this only errs if duplicate is sequential. later added duplicates seem to get a new object id.. 
+  // TODO: look deeper and make insert handler to manage this if needed.
+  //    },
+  //    {
+  //      _id: new ObjectId('68c9b7c156eb17a2b998fcec'),
+  //      id: 1,
+  //      label: 'title'
+  //    },
+  //    {
+  //      _id: new ObjectId('68c9b93af4f6bbb806ae0f21'),
+  //      id: 1,
+  //      label: 'title'
+  //    },
+
+  // const findResult = await collection.find().toArray();// didnt need the empty object
+  // console.log('Found documents =>', findResult);
+
+  // mongo.client.close();
 
 // }
 // catch (e) {
@@ -148,5 +165,5 @@ export default Mongo;
 //     console.log("NOPE", e.errmsg);
 //   else console.log("NOPE", e);
 // };
-// mongo.client.close();
 
+// mongo.client.close();
