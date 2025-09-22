3075d52993a45026350ef99bb061317442d32209
goodsie <mgoodie@outlook.com>
2025-09-22 14:15:31 -0400

    removed authjs and other unused items.

--- components/lib/types/app.types.ts
+++ components/lib/types/app.types.ts
@@ -1,81 +1,46 @@
+// app pages
 
 /**
- * Represents the accepted email format.
+ * Header
  */
-type AcceptedEmailFormat = string; // need to use a check
-
-/**
- * Represents the data from registration.
- */
-interface RegistrationData {
-  userName: string,
-  firstName: string,
-  lastName: string,
-  password?: string | Buffer,
-  email: AcceptedEmailFormat,
-}
-
-/**
-   * Represents a user account.
-   */
-interface AccountData extends RegistrationData {
-  uid: string,
-  auid: string,
-  hash: string,
-  verified: number,
-  tier: number,
-  isAdmin: number,
-  created_at?: string,
-}
-
-/**
- * Represents a tier object.
- */
-interface TierObject {
-  id: number,
-  name: string,
-  projects: number,
-  disk_quota: number,
-  clients: number,
-}
-
-
-/**
- * app pages
- */
-
 type NavLinks = [string, string][]
 interface HeaderPageData {
   navlinks: NavLinks;
 };
 
-
+/**
+ * About
+ */
 type Skills = string[];
 interface AboutPageData {
   skills: Skills;
 };
 
-
+/**
+ * Projects
+ */
 type Repos = { url: string }[];
 interface ProjectPageData {
   repos: Repos;
 };
 
-
+/**
+ * Services
+ */
 type Service = {
   title: string;
   description: string;
   technologies: string[],
   icon: string;
 }[];
-
 interface ServicesPageData {
   intro: string;
   offered: Service
 };
 
-
-
+/**
+ * Secret
+ */
 type Scripts = {
   title: string;
   code: string;
@@ -85,8 +50,9 @@ interface EggPageData {
   scripts: Scripts;
 };
 
-
-
+/**
+ * SiteData
+ */
 interface SiteData {
   projects: ProjectPageData;
   header: HeaderPageData;
