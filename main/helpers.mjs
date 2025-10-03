361b087c72c5a3c1e961a781a6a4a085c66015b7
goodsie <mgoodie@outlook.com>
2025-10-02 22:42:11 -0400

    extracted utility function . updated to latest netService. utilize middleware

---
+++ main/helpers.mjs
@@ -0,0 +1,35 @@
+import NetService from 'netservice';
+import logger from './logger.mjs';
+import chalk from 'chalk';
+
+/**
+ * @param {NetService} NetService
+ * 
+ * @private
+ */
+export async function gracefulShutdown(NetService) {
+  try {
+    NetService.Server.closeAllConnections();
+    await NetService.NextServer.close();
+    logger().info(chalk.yellowBright('<< NextServer Offline >>'));
+
+    NetService.Server.close(() => {
+      logger().info(chalk.greenBright('<< Exiting Normally >>'));
+      process.exit(0);
+    });
+
+  }
+  catch (e) {
+    logger().error('Error closing NetService:', e);
+    NetService.Server.close(() => {
+      logger().info(chalk.redBright('Exiting with code: 1 ->>'));
+      process.exit(1);
+    });
+  };
+
+  // force close connected clients
+  setTimeout(() => {
+    logger().info(chalk.yellowBright('Force closing connections..'));
+    process.exit(0);
+  }, 5000);
+};
--- main/index.mjs
+++ main/index.mjs
@@ -5,13 +5,23 @@ import NetService from 'netservice';
 import logger from './logger.mjs';
 import readline from 'readline';
 import chalk from 'chalk';
+import { gracefulShutdown } from './helpers.mjs';
 
 try {
   // startup check, bail if no version info
   OnSocketBanner(await readFile('.version', { encoding: 'utf8' }));
 
   const netservice = new NetService(process.env.DOMAIN);
-  netservice.on('ready', async () => logger().info(chalk.greenBright('<< Ready >>')));
+  netservice
+    .on('ready', async () => logger().info(chalk.greenBright('<< Ready >>')));
+
+  netservice.MiddlewareMgr
+    .register('*', netservice.Safety.mwRateLimit())
+    .register('*', netservice.Safety.mwBlockList());
+
+  process
+    .on('SIGINT', async () => await gracefulShutdown(netservice))
+    .on('SIGTERM', async () => await gracefulShutdown(netservice));
 
   readline.createInterface({
     input: process.stdin,
@@ -23,43 +33,8 @@ try {
       }
     });
 
-  process.on('SIGINT', async () => { await gracefulShutdown(netservice) });
-  process.on('SIGTERM', async () => { await gracefulShutdown(netservice) });
-
-} catch (e) {
+}
+catch (e) {
   console.error(e);
   logger().error('Construction Error:', e);
-};
-
-
-/**
- * @param {NetService} NetService
- * 
- * @private
- */
-async function gracefulShutdown(NetService) {
-  try {
-    NetService.Service.closeAllConnections();
-    await NetService.NextServer.close();
-    logger().info(chalk.yellowBright('<< NextServer Offline >>'));
-
-    NetService.Service.close(() => {
-      logger().info(chalk.greenBright('<< Exiting Normally >>'));
-      process.exit(0);
-    });
-
-  }
-  catch (e) {
-    logger().error('Error closing NetService:', e);
-    NetService.Service.close(() => {
-      logger().info(chalk.redBright('Exiting with code: 1 ->>'));
-      process.exit(1);
-    });
-  };
-
-  // force close connected clients
-  setTimeout(() => {
-    logger().info(chalk.yellowBright('Force closing connections..'));
-    process.exit(0);
-  }, 5000);
 };
